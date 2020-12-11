6ff94d4489f56cce563ca8f020970c71b324bd3b
buriymishka <55351111+buriymishka@users.noreply.github.com>
2020-12-11 19:27:09 +0200

    make it better

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const routerRecords = require('./routes/record.route')
 const routerTokens = require('./routes/tokens.routes')
 const app = express()
 const port = process.env.PORT || 80
-// const hostname = 'localhost'
+const hostname = process.env.HOST || 'localhost'
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -17,7 +17,7 @@ app.use(cookieParser());
 
 app.use(function (req, res, next) {
 
-  res.setHeader('Access-Control-Allow-Origin', 'https://mihail-vue-crm.herokuapp.com'); // 3000 http://localhost:8080
+  res.setHeader('Access-Control-Allow-Origin', 'https://mihail-vue-crm.herokuapp.com/'); // 3000 http://localhost:8080
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,access_token');
   res.setHeader('Access-Control-Allow-Credentials', true);
@@ -36,6 +36,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
 
 })
