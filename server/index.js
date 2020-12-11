a647f5c7aba3542e48f7e75b4ec272138e236ddd
buriymishka <55351111+buriymishka@users.noreply.github.com>
2020-12-11 20:34:57 +0200

    make it better

--- server/index.js
+++ server/index.js
@@ -36,6 +36,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-app.listen(port, hostname, () => {
-  console.log('server started')
-})
+// app.listen(port, hostname, () => {
+//   console.log('server started')
+// })
